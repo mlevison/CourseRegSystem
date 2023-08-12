f16d269612c318fc4461c53ab18ad9040a054cd8
Mark Levison <mark@agilepainrelief.com>
2023-08-12 17:32:02 -0400

    added courseNum

--- src/main/java/CourseSession.java
+++ src/main/java/CourseSession.java
@@ -1,11 +1,17 @@
 
 public class CourseSession {
     private final String dept;
-    public CourseSession(String inDept, String courseNumber) {
+    private final String courseNum;
+    public CourseSession(String inDept, String inCourseNumber) {
         dept = inDept;
+        courseNum = inCourseNumber;
     }
 
     public String getDept() {
         return dept;
+    }
+
+    public String getCourseNum() {
+        return courseNum;
     }
 }
--- src/test/java/CourseSessionTest.java
+++ src/test/java/CourseSessionTest.java
@@ -7,11 +7,13 @@ public class CourseSessionTest {
     public void testENGLDept(){
         CourseSession session = new CourseSession("ENGL", "101");
         assertEquals("ENGL", session.getDept());
+        assertEquals("101", session.getCourseNum());
     }
 
     @Test
     public void testScienceDept() {
         CourseSession session = new CourseSession("SCIN", "202");
         assertEquals("SCIN", session.getDept());
+        assertEquals("202", session.getCourseNum());
     }
 }
